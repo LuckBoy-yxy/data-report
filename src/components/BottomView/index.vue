4eab023996ef1c4fa4fba72966272d706c030aff
yuanxiaoran <hmxs_hmbb@163.com>
2023-10-18 14:22:30 +0800

    组件划分已完成

---
+++ src/components/BottomView/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="">
+    bottom-view
+  </div>
+</template>
+
+<script>
+  export default {
+    name: ''
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,15 +1,38 @@
 <template>
   <div class="home">
-    data-report
+    <TopView />
+
+    <SalesView />
+
+    <BottomView />
+
+    <MapView />
   </div>
 </template>
 
 <script>
+  import TopView from '../components/TopView/index.vue'
+  import BottomView from '../components/BottomView/index.vue'
+  import MapView from '../components/MapView/index.vue'
+  import SalesView from '../components/SalesView/index.vue'
+
   export default {
-    name: 'Home'
+    name: 'Home',
+    components: {
+      TopView,
+      BottomView,
+      MapView,
+      SalesView
+    }
   }
 </script>
 
 <style lang="scss">
-
+.home {
+  padding: 20px;
+  width: 100%;
+  height: 100%;
+  background-color: #eee;
+  box-sizing: border-box;
+}
 </style>
