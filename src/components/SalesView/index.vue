904cb8c4c2b2d7ccea4e450f3990722b97b75b10
yuanxiaoran <hmxs_hmbb@163.com>
2023-10-19 19:12:33 +0800

    销售趋势图表和列表组件结构与样式开发已完成

--- src/components/SalesView/index.vue
+++ src/components/SalesView/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="sales-view">
-    <el-card shadow="hover">
+    <el-card
+      shadow="hover"
+      :body-style="{ padding: '0 0 20px 0' }"
+    >
       <template v-slot:header>
         <div class="menu-wrapper">
           <el-menu
@@ -42,7 +45,30 @@
       </template>
 
       <template>
+        <div class="sales-view-chart-wrapper">
+          <v-chart :options="chartOption" />
 
+          <div class="sales-view-list">
+            <div class="sales-view-title">排行榜</div>
+            <div class="list-item-wrapper">
+              <div
+                class="list-item"
+                v-for="item in rankData"
+                :key="item.no"
+              >
+                <div
+                  :class="['list-item-no', +item.no <= 3 ? 'top-no' : '']"
+                >
+                  {{ item.no }}
+                </div>
+
+                <div class="list-item-name">{{ item.name }}</div>
+
+                <div class="list-item-money">{{ item.money }}</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </template>
     </el-card>
   </div>
@@ -86,7 +112,127 @@
               }
             }
           ]
-        }
+        },
+        chartOption: {
+          title: {
+            text: '年度销售额',
+            textStyle: {
+              fontSize: 12,
+              color: '#666'
+            },
+            left: 25,
+            top: 20
+          },
+          xAxis: {
+            type: 'category',
+            data: [
+              '1月',
+              '2月',
+              '3月',
+              '4月',
+              '5月',
+              '6月',
+              '7月',
+              '8月',
+              '9月',
+              '10月',
+              '11月',
+              '12月'
+            ],
+            axisLine: {
+              lineStyle: {
+                color: '#999'
+              }
+            },
+            axisTick: {
+              alignWithLabel: true,
+              lineStyle: {
+                color: '#999'
+              }
+            },
+            axisLabel: {
+              color: '#333'
+            }
+          },
+          yAxis: {
+            axisLine: {
+              show: false
+            },
+            axisTick: {
+              show: false
+            },
+            splitLine: {
+              lineStyle: {
+                type: 'dotted',
+                color: '#eee'
+              }
+            }
+          },
+          grid: {
+            top: 70,
+            left: 60,
+            right: 60,
+            bottom: 50
+          },
+          series: [
+            {
+              type: 'bar',
+              barWidth: '35%',
+              data: [
+                200,
+                250,
+                300,
+                350,
+                400,
+                450,
+                500,
+                550,
+                600,
+                650,
+                700,
+                750,
+              ],
+              color: ['#3398DB']
+            }
+          ]
+        },
+        rankData: [
+          {
+            no: 1,
+            name: '肯德基',
+            money: '323,234'
+          },
+          {
+            no: 2,
+            name: '麦当劳',
+            money: '323,234'
+          },
+          {
+            no: 3,
+            name: '汉堡王',
+            money: '323,234'
+          },
+          {
+            no: 4,
+            name: '海底捞',
+            money: '323,234'
+          },
+          {
+            no: 5,
+            name: '西贝筱面村',
+            money: '323,234'
+          },
+          {
+            no: 6,
+            name: '德克士',
+            money: '323,234'
+          },
+          {
+            no: 7,
+            name: '真功夫',
+            money: '323,234'
+          }
+        ]
       }
     },
     methods: {
@@ -123,6 +269,59 @@
       height: 50px;
       .sales-view-date-picker {
         margin-left: 20px;
+      }
+    }
+  }
+
+  .sales-view-chart-wrapper {
+    display: flex;
+    height: 270px;
+    .echarts {
+      flex: 0 0 70%;
+      width: 70%;
+      height: 100%;
+    }
+    .sales-view-list {
+      overflow: hidden;
+      flex: 1;
+      width: 100%;
+      height: 100%;
+      .sales-view-title {
+        margin-top: 20px;
+        font-size: 12px;
+        font-weight: 500;
+        color: #666;
+      }
+      .list-item-wrapper {
+        margin-top: 15px;
+        .list-item {
+          display: flex;
+          align-items: center;
+          padding: 6px 20px 6px 0;
+          height: 20px;
+          font-size: 12px;
+          .list-item-no {
+            width: 20px;
+            height: 20px;
+            line-height: 20px;
+            text-align: center;
+            color: #333;
+            &.top-no {
+              font-weight: 500;
+              color: #fff;
+              border-radius: 50%;
+              background-color: #000;
+            }
+          }
+          .list-item-name {
+            margin-left: 10px;
+            color: #333;
+          }
+          .list-item-money {
+            flex: 1;
+            text-align: right;
+          }
+        }
       }
     }
   }
