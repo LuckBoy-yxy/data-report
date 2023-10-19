22421f88d57d34de899e5d90fa320c52b34b9fec
yuanxiaoran <hmxs_hmbb@163.com>
2023-10-19 18:23:24 +0800

    开发销售趋势组件头部结构样式开发已完成

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -4,8 +4,20 @@ import {
   Card,
   Row,
   Col,
+  Menu,
+  MenuItem,
+  Radio,
+  RadioGroup,
+  RadioButton,
+  DatePicker
 } from 'element-ui'
 
 Vue.use(Card)
 Vue.use(Row)
 Vue.use(Col)
+Vue.use(Menu)
+Vue.use(MenuItem)
+Vue.use(Radio)
+Vue.use(RadioGroup)
+Vue.use(RadioButton)
+Vue.use(DatePicker)
